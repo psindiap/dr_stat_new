e6c1d40174bffa64324ee0dcf2981dddd9e39255
unknown <F20200043@pilani.bits-pilani.ac.in>
2023-02-17 13:46:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {useEffect,useState} from 'react'
+import Main from './Main/Main';
+
+function App() {
+
+
+
+  return (
+    <div>
+      <Main />
+      </div>
+  )
+}
+
+export default App
